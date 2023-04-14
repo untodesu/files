7011d7a28131db6020150cca2f7c294dfecd5b7b
Kirill GPRB <41549137+undnull@users.noreply.github.com>
2023-04-14 22:31:46 +0500

    New wallpaper directory

--- i3/wallpaper.sh
+++ i3/wallpaper.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# UNDONE: randomize the wallpaper
-# every time this command gets executed
-feh --bg-fill --randomize ${HOME}/.config/i3/wallpapers
+# UNDONE: do I want more wallpapers?
+feh --bg-fill --randomize ${HOME}/.config/wallpapers
